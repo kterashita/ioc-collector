77da5928b8e50e97005f6c4a7d1db0da63256af7
kterashita <804320+kterashita@users.noreply.github.com>
2021-12-07 16:01:49 +0900

    added some code for urlscanio and twitter

--- ioc-collector.py
+++ ioc-collector.py
@@ -4,6 +4,7 @@ import argparse
 import json
 import yaml
 import requests
+import tweepy
 
 init_yaml_filename = "config.yaml.init"
 
@@ -11,7 +12,9 @@ def get_argparse():
     parser = argparse.ArgumentParser(
         description="Help text of this command."
     )
-    parser.add_argument('--init', action='store_true')
+    parser.add_argument('--init', action='store_true', help="initalize config.yaml.init")
+    parser.add_argument('--urlscanio', action='store_true', help="urlscan.io submit")
+    parser.add_argument('--twitter', action='store_true', help="twitter search")
     parser.add_argument('-c', '--config', type=str, required=False,
                         help="yaml file")
     parser.add_argument('-u', '--url', type=str, required=False,
@@ -23,7 +26,11 @@ def init_yaml():
     init_yaml = {
         'twitter':
             {
-                'username': "", 'password': ""
+                'apikey': "",
+                'apikeysecret': "",
+                'accesstoken': "",
+                'accesstokensecret': "",
+                'bearer': ""
                 },
         'urlscanio':
             {
@@ -67,13 +74,41 @@ def run_urlscanio(args, config_dict):
     print(response.json())
 
 
+def run_twitter(args, config_dict):
+    #
+    # Ref: https://docs.tweepy.org/en/stable/getting_started.html
+    #
+    """
+    auth = tweepy.AppAuthHandler(
+        config_dict['twitter']['apikey'],
+        config_dict['twitter']['apikeysecret']
+        )
+    # auth.set_access_token(config_dict['twitter']['accesstoken'], config_dict['twitter']['accesstokensecret'])
+    api = tweepy.API(auth)
+    for tweet in tweepy.Cursor(api.search_tweets, q='tweepy').items(10):
+        print(tweet.text)
+    
+
+    client = tweepy.Client(
+        bearer_token=config_dict['twitter']['bearer'],
+        consumer_key=config_dict['twitter']['apikey'],
+        consumer_secret=config_dict['twitter']['apikeysecret'],
+        access_token=config_dict['twitter']['accesstoken'],
+        access_token_secret=config_dict['twitter']['accesstokensecret']
+        )
+    """
+
+
 def main():
     args = get_argparse()
     if args.init:
         init_yaml()
     if args.config:
         config_dict = load_config(args)
-    run_urlscanio(args, config_dict)
+    if args.urlscanio:
+        run_urlscanio(args, config_dict)
+    if args.twitter:
+        run_twitter(args, config_dict)
 
 
 if __name__ == '__main__':
